7e5677b1d521c45bdeacc241d215a4ac0b30bcae
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-16 19:39:32 +0100

    Brew formula update for meeting-mood version v0.2.0

--- meeting-mood.rb
+++ meeting-mood.rb
@@ -5,25 +5,25 @@
 class MeetingMood < Formula
   desc "Web app to collect people's moods in a meeting"
   homepage "https://github.com/wheresalice/meeting-mood"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/WheresAlice/meeting-mood/releases/download/v0.1.0/meeting-mood_Darwin_x86_64.tar.gz"
-    sha256 "d0a51ff95b2f44e4884969a1580c108a2a5d7a5f3f6ee6e4ba7875b1f8f71d78"
+    url "https://github.com/WheresAlice/meeting-mood/releases/download/v0.2.0/meeting-mood_Darwin_x86_64.tar.gz"
+    sha256 "54fbef89b8c30420c164b5c8d2a2dde402f89afacd1e4e0e31629f13d0b6f5b7"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/WheresAlice/meeting-mood/releases/download/v0.1.0/meeting-mood_Darwin_arm64.tar.gz"
-    sha256 "91f8e9b2fc801dcd1e262e40a0c5962c0d19c0ba4c03d4220bbe57021ca9773a"
+    url "https://github.com/WheresAlice/meeting-mood/releases/download/v0.2.0/meeting-mood_Darwin_arm64.tar.gz"
+    sha256 "fd5e86fbfa8020dc154ef6cacd66ce42209310f8c8fe84963c9d0375915b4851"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/WheresAlice/meeting-mood/releases/download/v0.1.0/meeting-mood_Linux_x86_64.tar.gz"
-    sha256 "12edc5ec6b16d0fa46928afb574969e9fb34aa7c008c131e4d9f20b2100f06c4"
+    url "https://github.com/WheresAlice/meeting-mood/releases/download/v0.2.0/meeting-mood_Linux_x86_64.tar.gz"
+    sha256 "9d58b64adbed9b162718a6b810d210290618404e24ba9d7a5eb69070c53c2f65"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/WheresAlice/meeting-mood/releases/download/v0.1.0/meeting-mood_Linux_arm64.tar.gz"
-    sha256 "4622198094eefbd8c5bd95ad0fb6f6dadc86d4528abb7712302187b1120f41d8"
+    url "https://github.com/WheresAlice/meeting-mood/releases/download/v0.2.0/meeting-mood_Linux_arm64.tar.gz"
+    sha256 "6bb0016c304df21b5c6559d048c113517cf995992bb8a45503965c81d7dd1626"
   end
 
   def install
